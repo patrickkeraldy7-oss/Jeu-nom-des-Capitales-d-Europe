45d8a7c84accc85b31854a40b12a74ce08e113df
patrickkeraldy7-oss <patrickkeraldy7@gmail.com>
2026-09-13 18:56:35 +0400

    Add files via upload

---
+++ jeu-les-capitales-europeennes/menu.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu");
+
+const aMenuUn = document.createElement("a");
+const aUn = "";
+
+aMenuUn.href = "index.html";
+aMenuUn.id = "index";
+aMenuUn.title = "Page d'accueil"
+aMenuUn.textContent = "Accueil";
+
+
+menu.appendChild(aMenuUn);
