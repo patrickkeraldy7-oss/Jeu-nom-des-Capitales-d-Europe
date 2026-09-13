9bcb7364db8435366fdec058b647fa52a5864793
patrickkeraldy7-oss <patrickkeraldy7@gmail.com>
2026-09-13 19:38:50 +0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById("menu");
+
+const aMenuUn = document.createElement("a");
+const aUn = "";
+
+aMenuUn.href = "index.html";
+aMenuUn.id = "index";
+aMenuUn.title = "Page d'accueil"
+aMenuUn.textContent = "Accueil";
+menu.appendChild(aMenuUn);
